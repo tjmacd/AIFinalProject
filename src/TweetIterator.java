b93f0d273267332e2cc4421c6acb2024b4a80ae4
TJ MacDougall <timothy.macdougall@uoit.net>
2017-04-03 02:37:27 -0400

    Some work in TweetIterator; created a dataset for testing purposes

--- src/TweetIterator.java
+++ src/TweetIterator.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
@@ -7,45 +12,77 @@ import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 public class TweetIterator implements DataSetIterator {
 	
 	private final int batchSize;
+	private int cursor = 0;
+	private final List<String> labels;
+	private int tweetCount = 0;
+	private final List<Pair<String, List<String>>> categoryData = new ArrayList<>();
 	
 	TweetIterator(int batchSize, boolean train){
 		this.batchSize = batchSize;
+		this.labels = new ArrayList<>();
+		this.loadData(train);
+		for(Pair<String, List<String>> datum : categoryData){
+			this.labels.add(datum.getKey());
+		}
+	}
+	
+	/*
+	 * Loads data from files for testing
+	 */
+	private void loadData(boolean train) {
+		readFile("fry", train);
+		readFile("cbc", train);
+	}
+	
+	private void readFile(String name, boolean train){
+		String filename;
+		if(train){
+			filename = name + "Train.txt";
+		} else {
+			filename = name + "Test.txt";
+		}
+		try(BufferedReader br = new BufferedReader(new FileReader(filename))){
+			String line = "";
+			List<String> tempList = new ArrayList<>();
+			while((line = br.readLine()) != null) {
+				tempList.add(line);
+				this.tweetCount++;
+			}
+			Pair<String, List<String>> tempPair = Pair.of(name, tempList);
+			this.categoryData.add(tempPair);
+		} catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		return false;
+		return cursor < numExamples();
 	}
 
 	@Override
 	public DataSet next() {
-		// TODO Auto-generated method stub
-		return null;
+		return next(batchSize);
 	}
 
 	@Override
 	public boolean asyncSupported() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public int batch() {
-		// TODO Auto-generated method stub
-		return 0;
+		return batchSize;
 	}
 
 	@Override
 	public int cursor() {
-		// TODO Auto-generated method stub
-		return 0;
+		return cursor;
 	}
 
 	@Override
 	public List<String> getLabels() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.labels;
 	}
 
 	@Override
@@ -61,15 +98,14 @@ public class TweetIterator implements DataSetIterator {
 	}
 
 	@Override
-	public DataSet next(int arg0) {
+	public DataSet next(int num) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int numExamples() {
-		// TODO Auto-generated method stub
-		return 0;
+		return totalExamples();
 	}
 
 	@Override
