918dd1200ab998427260fecdd24448a22e41f477
TJ MacDougall <timothy.macdougall@uoit.net>
2017-04-05 16:52:35 -0400

    Classifier works for News example data

--- src/TweetClassifier.java
+++ src/TweetClassifier.java
@@ -1,31 +1,51 @@
+import java.io.File;
+
 import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.GradientNormalization;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.GravesLSTM;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
 
 public class TweetClassifier {
-	public void train(){
+	
+	public static WordVectors wordVectors;
+	
+	public static void main(String[] args) throws Exception {
 		int outputNum = 2;   // number of output classes
-		int batchSize = 128; // batch size for each epoch
+		int batchSize = 50; // batch size for each epoch
 		int rngSeed = 123;   // random number seed for reproducibility
-		int numEpochs = 15;  // number of epochs to perform
-		int numInputs = 0;
-		int numHiddenNodes = 0;
+		int numEpochs = 1000;  // number of epochs to perform
 		
-		DataSetIterator iTrain = new TweetIterator(batchSize, true);
-		DataSetIterator iTest = new TweetIterator(batchSize, false);
 		
+		String DATA_PATH = "LabelledData";
+		String WORD_VECTORS_PATH = "TweetWordVector.txt";
+		
+		wordVectors = WordVectorSerializer.loadTxtVectors(new File(WORD_VECTORS_PATH));
+		
+		DataSetIterator iTrain = new TweetIterator(wordVectors, batchSize, true);
+		DataSetIterator iTest = new TweetIterator(wordVectors, batchSize, false);
+		
+		int numInputs = wordVectors.getWordVector(wordVectors.vocab().wordAtIndex(0)).length;
+		int numHiddenNodes = 200;
+		
+		/*
 		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
 				.seed(rngSeed) // random seed for reproducibility
 				// use stochastic gradient descent as optimization algorithm
@@ -51,6 +71,24 @@ public class TweetClassifier {
 						.build())
 				.pretrain(false).backprop(true) // adjust weights with backpropagation
 				.build();
+				*/
+		
+		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+	            .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(1)
+	            .updater(Updater.RMSPROP)
+	            .regularization(true).l2(1e-5)
+	            .weightInit(WeightInit.XAVIER)
+	            .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue).gradientNormalizationThreshold(1.0)
+	            .learningRate(0.0018)
+	            .list()
+	            .layer(0, new GravesLSTM.Builder()
+	            		.nIn(numInputs)
+	            		.nOut(200)
+	            		.activation(Activation.SOFTSIGN)
+	            		.build())
+	            .layer(1, new RnnOutputLayer.Builder().activation(Activation.SOFTMAX)
+	                .lossFunction(LossFunctions.LossFunction.MCXENT).nIn(200).nOut(outputNum).build())
+	            .pretrain(false).backprop(true).build();
 		
 		MultiLayerNetwork model = new MultiLayerNetwork(conf);
 		model.init();
@@ -59,6 +97,7 @@ public class TweetClassifier {
 		System.out.println("Train model...");
 		for(int i=0; i<numEpochs; i++) {
 			model.fit(iTrain);
+			iTrain.reset();
 		}
 		
 		System.out.println("Evaluate model...");
--- src/TweetIterator.java
+++ src/TweetIterator.java
@@ -1,13 +1,24 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.INDArrayIndex;
+
+import static org.nd4j.linalg.indexing.NDArrayIndex.*;
 
 public class TweetIterator implements DataSetIterator {
 	
@@ -16,44 +27,61 @@ public class TweetIterator implements DataSetIterator {
 	private final List<String> labels;
 	private int tweetCount = 0;
 	private final List<Pair<String, List<String>>> categoryData = new ArrayList<>();
+	private final int vectorSize;
+	private int currCategory = 0;
+	private int tweetPos = 0;
+	private final TokenizerFactory tokenizerFactory;
+	private final WordVectors wordVectors;
+	private final int truncateLength = 300;
 	
-	TweetIterator(int batchSize, boolean train){
+	TweetIterator(WordVectors wordVectors, int batchSize, boolean train){
 		this.batchSize = batchSize;
 		this.labels = new ArrayList<>();
 		this.loadData(train);
 		for(Pair<String, List<String>> datum : categoryData){
-			this.labels.add(datum.getKey());
+			this.labels.add(datum.getKey().split(",")[1]);
 		}
+		this.vectorSize = wordVectors.getWordVector(
+				wordVectors.vocab().wordAtIndex(0)).length;
+		this.wordVectors = wordVectors;
+		tokenizerFactory = new DefaultTokenizerFactory();
+        tokenizerFactory.setTokenPreProcessor(new CommonPreprocessor());
 	}
 	
 	/*
 	 * Loads data from files for testing
 	 */
 	private void loadData(boolean train) {
-		readFile("fry", train);
-		readFile("cbc", train);
-	}
-	
-	private void readFile(String name, boolean train){
-		String filename;
-		if(train){
-			filename = name + "Train.txt";
-		} else {
-			filename = name + "Test.txt";
-		}
-		try(BufferedReader br = new BufferedReader(new FileReader(filename))){
-			String line = "";
-			List<String> tempList = new ArrayList<>();
-			while((line = br.readLine()) != null) {
-				tempList.add(line);
-				this.tweetCount++;
-			}
-			Pair<String, List<String>> tempPair = Pair.of(name, tempList);
-			this.categoryData.add(tempPair);
+		String dataDirectory = "LabelledData";
+		File categories = new File(dataDirectory + File.separator + "categories.txt");
+		
+		try(BufferedReader brCategories = new BufferedReader(new FileReader(categories))){
+			String temp = "";
+            while ((temp = brCategories.readLine()) != null) {
+                String curFileName = train == true ?
+                    dataDirectory + File.separator + "train" + File.separator + temp.split(",")[0] + ".txt" :
+                    dataDirectory + File.separator + "test" + File.separator + temp.split(",")[0] + ".txt";
+                File currFile = new File(curFileName);
+                BufferedReader currBR = new BufferedReader((new FileReader(currFile)));
+                String tempCurrLine = "";
+                List<String> tempList = new ArrayList<>();
+                while ((tempCurrLine = currBR.readLine()) != null) {
+                	if(!tempCurrLine.isEmpty()){
+                		tempList.add(tempCurrLine);
+                		tweetCount++;
+                	}
+                }
+                currBR.close();
+                System.out.println(temp + ", " + tweetCount);
+                Pair<String, List<String>> tempPair = Pair.of(temp, tempList);
+                this.categoryData.add(tempPair);
+            }
+            brCategories.close();
 		} catch (IOException e){
 			e.printStackTrace();
 		}
 	}
+	
 
 	@Override
 	public boolean hasNext() {
@@ -87,20 +115,95 @@ public class TweetIterator implements DataSetIterator {
 
 	@Override
 	public DataSetPreProcessor getPreProcessor() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int inputColumns() {
-		// TODO Auto-generated method stub
-		return 0;
+		return vectorSize;
 	}
 
 	@Override
 	public DataSet next(int num) {
-		// TODO Auto-generated method stub
-		return null;
+		if (cursor >= this.tweetCount)
+			throw new NoSuchElementException();
+		try {
+			return nextDataSet(num);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private DataSet nextDataSet(int num) throws IOException {
+		List<String> tweets = new ArrayList<>(num);
+		int[] category = new int[num];
+		System.out.println("142: " + currCategory + ", "+ categoryData.size());
+
+		for(int i=0; i< num && cursor < totalExamples(); i++) {
+			System.out.println("tweetPos: "+tweetPos);
+			if(currCategory < categoryData.size()) {
+				tweets.add(categoryData.get(currCategory).getValue()
+						.get(tweetPos));
+				category[i] = Integer.parseInt(categoryData.get(currCategory)
+						.getKey().split(",")[0]);
+				currCategory++;
+				cursor++;
+			} else {
+				currCategory = 0;
+				tweetPos++;
+				i--;
+			}
+		}
+		
+		// Tokenize strings and filter out unknown words
+		List<List<String>> allTokens = new ArrayList<>(tweets.size());
+		int maxLength = 0;
+		for(String s : tweets) {
+			//System.out.println(s);
+			List<String> tokens = tokenizerFactory.create(s).getTokens();
+			List<String> tokensFiltered = new ArrayList<>();
+			for(String t : tokens) {
+				if(wordVectors.hasWord(t))
+					tokensFiltered.add(t);
+			}
+			allTokens.add(tokensFiltered);
+			maxLength = Math.max(maxLength, tokensFiltered.size());
+		}
+		System.out.println("maxLength: " + maxLength + " trunc: "+ truncateLength);
+
+		if(maxLength > truncateLength){
+			maxLength = truncateLength;
+		}
+		
+		// Create data for training
+		INDArray features = Nd4j.create(tweets.size(), vectorSize, maxLength);
+		INDArray labels = Nd4j.create(tweets.size(), this.categoryData.size(), 
+				maxLength);
+		
+		INDArray featuresMask = Nd4j.zeros(tweets.size(), maxLength);
+		INDArray labelsMask = Nd4j.zeros(tweets.size(), maxLength);
+		
+		int[] temp = new int[2];
+		for(int i = 0; i < tweets.size(); i++) {
+			List<String> tokens = allTokens.get(i);
+			temp[0] = i;
+			// Get word vectors for each word and put them in the training data
+			for (int j = 0; j < tokens.size() && j < maxLength; j++) {
+				String token = tokens.get(j);
+				INDArray vector = wordVectors.getWordVectorMatrix(token);
+				features.put(new INDArrayIndex[] {point(i),  all(), point(j)},
+						vector);
+				temp[1] = j;
+				featuresMask.putScalar(temp, 1.0);
+			}
+			int idx = category[i];
+			int lastIdx = Math.min(tokens.size(), maxLength);
+			labels.putScalar(new int[]{i, idx, lastIdx-1}, 1.0);
+			labelsMask.putScalar(new int[]{i,  lastIdx-1}, 1.0);
+		}
+		
+		DataSet ds = new DataSet(features, labels, featuresMask, labelsMask);
+		return ds;
 	}
 
 	@Override
@@ -110,32 +213,31 @@ public class TweetIterator implements DataSetIterator {
 
 	@Override
 	public void reset() {
-		// TODO Auto-generated method stub
+		cursor = 0;
+		tweetPos = 0;
+		currCategory = 0;
 
 	}
 
 	@Override
 	public boolean resetSupported() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public void setPreProcessor(DataSetPreProcessor arg0) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
 
 	}
 
 	@Override
 	public int totalExamples() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.tweetCount;
 	}
 
 	@Override
 	public int totalOutcomes() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.categoryData.size();
 	}
 
 }
